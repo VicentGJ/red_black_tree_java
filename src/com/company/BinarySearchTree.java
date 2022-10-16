67aa20c7e98c00ca63e9d195dbfbd5bb29703242
Vicente <Vicente Garofalo>
2022-10-16 16:58:41 -0400

    Initial Commit

---
+++ src/com/company/BinarySearchTree.java
@@ -0,0 +1,82 @@
+package com.company;
+
+public class BinarySearchTree {
+    private Node<Integer> root;
+
+    public Node<Integer> search(int value) {
+        return this.search(this.root, value);
+    }
+
+    private Node<Integer> search(Node<Integer> node, int value)
+    {
+        if (node==null || node.getValue() == value) {
+            return node;
+        }
+        if (node.getValue() < value) {
+            return search(node.getRight(), value);
+        }
+        return search(node.getLeft(), value);
+    }
+
+    public void insert(int value) {
+        this.root = this.insert(this.root, value);
+    }
+
+    private Node<Integer> insert(Node<Integer> node, int value) {
+        if (node == null) {
+            node = new Node<>(value);
+            return node;
+        }
+        if (value < node.getValue())
+            node.setLeft(insert(node.getLeft(), value));
+        else if (value > node.getValue())
+            node.setRight(insert(node.getRight(), value));
+
+        return node;
+    }
+
+    public void delete(int value) {
+        this.root = delete(this.root, value);
+    }
+
+    private Node<Integer> delete(Node<Integer> node, int value) {
+        if (node == null) return null;
+        if (value < node.getValue())
+            node.setLeft(delete(node.getLeft(), value));
+        else if (value > node.getValue())
+            node.setRight(delete(node.getRight(), value));
+
+        else {
+            if (node.getLeft()==null)
+                return node.getRight();
+            else if (node.getRight() == null) {
+                return node.getLeft();
+            }
+            node.setValue(minValue(node.getRight()));
+            node.setRight(delete(node.getRight(),node.getValue()));
+        }
+        return node;
+    }
+
+    private int minValue(Node<Integer> node) {
+        int min = node.getValue();
+        while (node.getLeft() != null) {
+            min = node.getLeft().getValue();
+            node = node.getLeft();
+        }
+        return min;
+    }
+
+    // This method mainly calls InorderRec()
+    public void inorder() { inorderRec(this.root); }
+
+    // A utility function to do inorder traversal of BST
+    private void inorderRec(Node<Integer> node)
+    {
+        if (node != null) {
+            inorderRec(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            inorderRec(node.getLeft());
+        }
+    }
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Node<E> {
+    private Node<E> left;
+    private Node<E> right;
+    private E value;
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+}
